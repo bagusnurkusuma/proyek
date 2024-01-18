d530573da4793317466b306a47c96977782ca414
Bagus Nur Kusuma <75839127+bagusnurkusuma@users.noreply.github.com>
2024-01-18 10:59:13 +0700

    standarisasi btn

--- general_journal/form.php
+++ general_journal/form.php
@@ -26,13 +26,10 @@ include "api.php";
               <div class="x_title">
                 <h2>General Journal</h2>
                 <ul class="nav navbar-right panel_toolbox">
-                  <li><a class="collapse-link"><i class="fa fa-chevron-up justify-content-end"></i></a>
-                  </li>
+                  <li><button type="button" name="pay" id="jq_pay" class="btn btn-success save_transaction">Save</button></li>
+                  <li><button type="button" name="cancel" id="jq_cancel" class="btn btn-primary cancel" onclick="act_cancel()">Cancel</button></li>
+                  <li><a class="collapse-link"><i class="fa fa-chevron-up justify-content-end"></i></a></li>
                 </ul>
-                <div align="right">
-                  <button type="button" name="pay" id="jq_pay" class="btn btn-success save_transaction">Save</button>
-                  <button type="button" name="cancel" id="jq_cancel" class="btn btn-primary cancel" onclick="act_cancel()">Cancel</button>
-                </div>
                 <div class="clearfix"></div>
               </div>
               <div class="x_content">
@@ -75,12 +72,10 @@ include "api.php";
                     <div class="x_title">
                       <h2>Account Detail </h2>
                       <ul class="nav navbar-right panel_toolbox">
+                        <li><button type="button" name="add_data" id="jq_add_data" class="btn btn-warning add_data"><i class="fa fa-plus-circle"></i></button></li>
+                        <li><button type="button" name="refresh" id="jq_refresh" class="btn btn-success refresh_data"><i class="fa fa-refresh"></i></button></li>
                         <li><a class="collapse-link"><i class="fa fa-chevron-up"></i></a></li>
                       </ul>
-                      <div align="right">
-                        <button type="button" name="add_data" id="jq_add_data" class="btn btn-warning add_data"><i class="fa fa-plus-circle"></i></button>
-                        <button type="button" name="refresh" id="jq_refresh" class="btn btn-success refresh_data"><i class="fa fa-refresh"></i></button>
-                      </div>
                       <div class="clearfix"></div>
                     </div>
                     <div class="x_content">
